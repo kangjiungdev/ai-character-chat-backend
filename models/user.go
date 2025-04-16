f716c2bb7963eeb466d7eea96033355c6d51b3ef
kangjiungdev <kangjiungdev@gmail.com>
2025-04-16 15:26:38 +0900

    gorm edit

--- models/user.go
+++ models/user.go
@@ -25,6 +25,10 @@ type User struct {
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
 
+func (User) TableName() string {
+	return "users"
+}
+
 type UserFormRequest struct {
 	ID          string `json:"id" binding:"required" form:"user-id"`
 	Password    string `json:"password" binding:"required" form:"password"`
@@ -36,27 +40,27 @@ type UserFormRequest struct {
 func (u *UserFormRequest) UserFormValidation() error {
 	inputRegex := regexp.MustCompile(`^[a-zA-Z0-9]+$`)
 	if !inputRegex.MatchString(u.ID) {
-		return fmt.Errorf("아이디는 영어와 숫자만 입력 가능합니다")
+		return errors.New("아이디는 영어와 숫자만 입력 가능합니다")
 	}
 	if utf8.RuneCountInString(u.ID) < 6 || utf8.RuneCountInString(u.ID) > 15 {
-		return fmt.Errorf("아이디는 6자~15자여야 합니다")
+		return errors.New("아이디는 6자~15자여야 합니다")
 	}
 
 	if !inputRegex.MatchString(u.Password) {
-		return fmt.Errorf("비밀번호는 영어와 숫자만 입력 가능합니다")
+		return errors.New("비밀번호는 영어와 숫자만 입력 가능합니다")
 	}
 	if utf8.RuneCountInString(u.Password) < 8 || utf8.RuneCountInString(u.Password) > 20 {
-		return fmt.Errorf("비밀번호는 8자~20자여야 합니다")
+		return errors.New("비밀번호는 8자~20자여야 합니다")
 	}
 
 	phoneRegex := regexp.MustCompile(`^\d{3}-\d{4}-\d{4}$`)
 	if !phoneRegex.MatchString(u.PhoneNumber) {
-		return fmt.Errorf("전화번호 형식이 올바르지 않습니다")
+		return errors.New("전화번호 형식이 올바르지 않습니다")
 	}
 
 	_, err := time.Parse("2006-01-02", u.BirthDate)
 	if err != nil {
-		return fmt.Errorf("생년월일 형식이 올바르지 않습니다")
+		return errors.New("생년월일 형식이 올바르지 않습니다")
 	}
 	return nil
 }
@@ -66,7 +70,7 @@ func CreateUser(db *gorm.DB, user *User) (int, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		fmt.Println("비밀번호 해시 에러:", err)
-		return http.StatusInternalServerError, fmt.Errorf("요청을 처리하는 중 문제가 발생했습니다. 나중에 다시 시도해주세요")
+		return http.StatusInternalServerError, errors.New("요청을 처리하는 중 문제가 발생했습니다. 나중에 다시 시도해주세요")
 	}
 	user.Password = string(hashedPassword)
 
@@ -75,9 +79,9 @@ func CreateUser(db *gorm.DB, user *User) (int, error) {
 	if err != nil {
 		// 중복 ID 에러 확인
 		if isDuplicateKeyError(err) {
-			return http.StatusConflict, fmt.Errorf("해당 아이디를 가진 유저가 이미 존재합니다")
+			return http.StatusConflict, errors.New("해당 아이디를 가진 유저가 이미 존재합니다")
 		}
-		return http.StatusInternalServerError, fmt.Errorf("요청을 처리하는 중 문제가 발생했습니다. 나중에 다시 시도해주세요")
+		return http.StatusInternalServerError, errors.New("요청을 처리하는 중 문제가 발생했습니다. 나중에 다시 시도해주세요")
 	}
 
 	return http.StatusCreated, nil
@@ -86,7 +90,7 @@ func CreateUser(db *gorm.DB, user *User) (int, error) {
 func DeleteUser(db *gorm.DB, userID string) error {
 	err := db.Where("id = ?", userID).Delete(&User{}).Error
 	if err != nil {
-		return fmt.Errorf("계정 삭제 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요")
+		return errors.New("계정 삭제 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요")
 	}
 	return nil
 }
